c783d8d1f9a4d9a57b7e5cf897bf8988c2d811e9
dhfra21 <dhiafrad2001@gmail.com>
2025-05-28 15:37:33 +0100

    Initial commit from note-app contents

--- backend/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NotesModule } from './notes/notes.module';
-
-@Module({
-  imports: [NotesModule],
-})
-export class AppModule {}
--- backend/src/notes/dto/update-note.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNoteDto } from './create-note.dto';
-
-export class UpdateNoteDto {
-  title?: string;
-  content?: string;
-}
